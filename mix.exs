a2cdb0acb7905c1fbbfaa1490ee36f4acbd088d6
Jamie Hewland <jhewland@gmail.com>
2019-02-13 13:48:48 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExvaultUmbrella.MixProject do
   def project do
     [
       app: :exvault,
-      version: "0.1.0-beta.0",
+      version: "0.1.0-beta.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
